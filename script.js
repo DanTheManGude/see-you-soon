8d836809163b7a38eb22404bdae7a0989104bc40
Daniel Gude <dgude31@gmail.com>
2022-10-13 21:54:14 -0400

    Multiple events (#4)
    * Background and header color
    
    * Multiple events

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-const END_TIME = Date.parse("26 Oct 2022 1:33 PM EDT");
-// Make sure the date is formated correctly
+const events = {
+  "see-you-soon": {
+    endTime: Date.parse("26 Oct 2022 1:33 PM EDT"),
+    text: "we are together again",
+    color: "#a978f7",
+  },
+};
+
+const defaultEvent = {
+  endTime: Date.now(),
+  text: "...",
+  color: "#f2f2f2",
+};
 
 function zeroPad(num) {
   if (num >= 10) {
@@ -8,31 +19,47 @@ function zeroPad(num) {
   return `0${num}`;
 }
 
-function showTime() {
-  let now = Date.now();
-  let difference = Math.abs(END_TIME - now);
+function getShowTime(endTime) {
+  return () => {
+    let now = Date.now();
+    let difference = Math.abs(endTime - now);
 
-  let days = Math.floor(difference / (24 * 60 * 60 * 1000));
-  difference = difference - days * 24 * 60 * 60 * 1000;
+    let days = Math.floor(difference / (24 * 60 * 60 * 1000));
+    difference = difference - days * 24 * 60 * 60 * 1000;
 
-  let hours = Math.floor(difference / (60 * 60 * 1000));
-  difference = difference - hours * 60 * 60 * 1000;
+    let hours = Math.floor(difference / (60 * 60 * 1000));
+    difference = difference - hours * 60 * 60 * 1000;
 
-  let min = Math.floor(difference / (60 * 1000));
-  difference = difference - min * 60 * 1000;
+    let min = Math.floor(difference / (60 * 1000));
+    difference = difference - min * 60 * 1000;
 
-  let seconds = Math.floor(difference / 1000);
-  difference = difference - seconds * 1000;
+    let seconds = Math.floor(difference / 1000);
+    difference = difference - seconds * 1000;
 
-  let tenths = Math.floor(difference / 100);
+    let tenths = Math.floor(difference / 100);
 
-  const displayTime = `${days}:${zeroPad(hours)}:${zeroPad(min)}:${zeroPad(
-    seconds
-  )}.${tenths}`;
-  const clockElement = document.getElementById("clock");
+    const displayTime = `${days}:${zeroPad(hours)}:${zeroPad(min)}:${zeroPad(
+      seconds
+    )}.${tenths}`;
+    const clockElement = document.getElementById("clock");
 
-  clockElement.innerHTML = displayTime;
-  clockElement.classList.add(END_TIME >= now ? "down" : "up");
+    clockElement.innerHTML = displayTime;
+    clockElement.classList.add(endTime >= now ? "down" : "up");
+  };
 }
 
-setInterval(showTime, 100);
+const path = window.location.pathname;
+const cleanPath = path.endsWith("/") ? path.slice(0, -1) : path;
+const eventKey = cleanPath.split("/").at(-1);
+
+const eventObj = events.hasOwnProperty(eventKey)
+  ? events[eventKey]
+  : defaultEvent;
+
+const { endTime, text, color } = eventObj;
+
+setInterval(getShowTime(endTime), 100);
+
+const headerElemnt = document.getElementById("header");
+headerElemnt.style.color = color;
+headerElemnt.innerHTML = `Time left until ${text}`;
